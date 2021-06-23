3be99e7f9e1f27107089873b8589e87fc4e947a0
jardelrs <jardelrs@gmail.com>
2021-06-23 17:09:31 -0400

    Criado o método pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,1 +1,9 @@
 class Pessoa:
+    def cumprimento(self):
+        return f'Olá mundo {id(p)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimento(p))
+    print(id(p))
+    print(p.cumprimento())
