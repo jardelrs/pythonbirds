a70aaf769630c87b3d5e56bdc1852a4ddc254a44
jardelrs <jardelrs@gmail.com>
2021-06-24 15:33:07 -0400

    Criado os atributos de nome e idade na classe pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=38):
+        self.idade = idade
+        self.nome = nome
     def cumprimento(self):
         return f'Olá mundo {id(p)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Pedro')
     print(Pessoa.cumprimento(p))
     print(id(p))
+    print(p.nome)
     print(p.cumprimento())
+    p.nome = 'Jardel'
+    print(p.nome)
+    print(p.idade)
